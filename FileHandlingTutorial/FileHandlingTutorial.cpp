c2c39a786104dfc1d18002ee43cca7554be21a74
mattburt36 <matt-burton@live.nmit.ac.nz>
2021-07-09 10:49:58 +1200

    Read file "Matt.txt" example

--- FileHandlingTutorial/FileHandlingTutorial.cpp
+++ FileHandlingTutorial/FileHandlingTutorial.cpp
@@ -6,18 +6,16 @@ using namespace std;
 int main()
 {
     fstream myFile; 
-    myFile.open("Matt.txt", ios::out);  // Write to file and create
+    myFile.open("Matt.txt", ios::in); // Read file to console
     if (myFile.is_open())
     {
-        myFile << "Hello\n"; 
-        myFile << "This is the second line\n";
+        string line; 
+        while (getline(myFile, line))
+        {
+            cout << line << endl; 
+        }
         myFile.close();
     }
-    myFile.open("Matt.txt", ios::app);  // Append, use this to add to end of file  
-    if (myFile.is_open())
-    {
-        myFile << "Appended Hello\n";
-        myFile.close();
-    }
+
     system("pause>0");
 }
